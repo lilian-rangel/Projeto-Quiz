48dfc258be765d0bc071d28f583ee63a797abe7b
lilian-rangel <lilianrm.nutri@gmail.com>
2022-11-07 09:26:09 -0300

    criação da pasta components/23ctd flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+// ordem de importação: react, componentes, estáticos
+import Welcome from './components/Welcome';
+
 import './App.css'
 
 function App() {
 
   return (
     <div className="App">
-  <h1>Quiz</h1>        
+  <h1>Quiz de programação</h1>     
+    <Welcome />   
     </div>
   );
 }
 
-export default App
+export default App;

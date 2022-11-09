ba40f05cf28594b8afc240e619e8b0259cd415f9
lilian-rangel <lilianrm.nutri@gmail.com>
2022-11-09 17:59:26 -0300

    criação de componentes/estilização css/mudança de estágio/25ctd

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 // ordem de importação: react, componentes, estáticos
-import Welcome from './components/Welcome';
 
-import './App.css'
+import { useContext } from 'react';
+import { QuizContext } from './context/quiz';
+
+import Welcome from './components/Welcome';
+import Question from './components/Question';
+
+import './App.css';
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
 
   return (
     <div className="App">
   <h1>Quiz de programação</h1>     
-    <Welcome />   
+    {quizState.gameStage === "Start" && <Welcome />}
+    {quizState.gameStage === "Playing" && <Question />}   
+   
     </div>
   );
 }
--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -1,9 +1,32 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
+import questions from "../data/questions"
 
-export const QuizContext = createContext()
+const STAGES = ["Start", "Playing", "End"]
+
+const initialState = {
+    gameStage: STAGES[0],
+    questions
+}
+
+const quizReducer = (state, action) => {
+    console.log(state, action);
+    
+    switch(action.type) {
+        case "CHANGE_STATE":
+            return {
+                ...state,
+                gameStage: STAGES[1],
+            };
+
+         default:
+            return state; 
+    }
+};
+
+export const QuizContext = createContext();
 
 export const QuizProvider = ({children}) => {
-    const value = { name : "Quiz"};
+    const value = useReducer(quizReducer, initialState);
 
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
